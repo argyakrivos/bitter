c5995fd6acc00cff940aa26fb208aa21bb107128
Anargyros Akrivos <akrivos@gmail.com>
2012-09-29 19:44:08 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitter::Application.config.secret_token = 'a93448c55f2b7f3fcd6cc23a6593544d59987f8c7367d18ae9880e33521ec89ea28c3ae5b1432a410c3d4e1a5ea6a5cd3ea448161c44bd0f6166bd9611277893'
