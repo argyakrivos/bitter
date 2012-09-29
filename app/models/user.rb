b5682587d6738c81b400f41f27ccaf7c050e8069
Mohammed Abdulrazeg <objectivemo@gmail.com>
2012-09-29 23:20:52 +0100

    Initial code for the user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User
   key :name, String
   key :email, String
 
-  validates_presence_of :name
+  validates_length_of :name, :within => 3..40
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
